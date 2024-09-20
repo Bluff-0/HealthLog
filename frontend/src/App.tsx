63a1c6bbf5175a30e88eb593577001c1a0deef86
Saptarshi Mazumdar <saptarshi.wd@gmail.com>
2024-09-20 21:51:20 +0530

    deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First Deployment Succeeded
+          First Deployment Succeeded!
         </p>
         <a
           className="App-link"
